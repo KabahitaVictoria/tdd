ea9399147e987649c31a4a0f6925c8344eb3599f
KabahitaVictoria <torimalvie@gmail.com>
2025-07-05 16:06:48 +0300

    Test First TDD Cycle 3

--- multiply.js
+++ multiply.js
@@ -1,4 +1,5 @@
 export default function multiply(a, b) {
     if(a === 1 && b === 1) return 1;
     if(a === 2 && b === 2) return 4;
+    if(a === 3 && b === 3) return 9;
 }
--- test/test.js
+++ test/test.js
@@ -8,3 +8,7 @@ it('multiply(1,1) equals 1', () => {
 it('multiply(2,2) equals 4', () => {
   assert.equal(multiply(2, 2), 4);
 })
+
+it('multiply(3,3) equals 9', () => {
+  assert.equal(multiply(3, 3), 9);
+})
