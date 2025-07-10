9a1a1eaa5408e2ac6c242be965cfaa04e9c6c732
KabahitaVictoria <torimalvie@gmail.com>
2025-07-10 11:30:53 +0300

    Second Cycle - Tax Calculator (Stretch Exercise)

--- taxCalculator/taxCalculator.js
+++ taxCalculator/taxCalculator.js
@@ -1,3 +1,10 @@
 export default function taxCalculator(earnings) {
-    return 0;
+    let tax = 0;
+
+    if(earnings < 12000) {
+        tax = 0;
+    } else if(earnings <= 36000) {
+        tax = 0.20 * (earnings - 12000);
+    }
+    return tax;
 }
--- taxCalculator/test/test.js
+++ taxCalculator/test/test.js
@@ -5,4 +5,12 @@ describe('Tax Calculator Tests', () => {
     it('taxCalculator(11999) should return 0 tax', () => {
         assert.equal(taxCalculator(11999), 0)
     })
+
+    it('taxCalculator(12000) should return 0 tax', () => {
+        assert.equal(taxCalculator(12000), 0)
+    })
+
+    it('taxCalculator(13000) should return 200', () => {
+        assert.equal(taxCalculator(13000), 200)
+    })
 })
